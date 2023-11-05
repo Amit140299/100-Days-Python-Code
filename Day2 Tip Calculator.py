8e745ce93f9c45002915e4778f3bf965e6e2292a
Amit140299 <104049987+Amit140299@users.noreply.github.com>
2023-11-05 10:52:09 +0000

    Completed second projet

---
+++ Day2 Tip Calculator.py
@@ -0,0 +1,13 @@
+# 1.Create a greeting for your program.
+print('Wlcome to Tip Calculator.')
+# 2.Ask the user for the total bill.
+Total_Bill=float(input("What was the total bill?\n"))
+# 3.Ask the user for number of people to split the bill.
+People=float(input('How many people to split the bill?\n'))
+# 4.Ask the user for percentage tip they would like to give.
+Tip_perecent=float(input('What percentage tip would you like to give?\n'))
+# 5. Calculate final bill per person.
+Final_bill=(Total_Bill+((Total_Bill*Tip_perecent)/100))/People
+# 6. Show them the amount per person.
+print('Each person should pay:',Final_bill)
+# 7.Make sure the input curesor shows on new line
