6b207280eb9004cd8f6b2fde6106eec47749d044
Amit140299 <104049987+Amit140299@users.noreply.github.com>
2023-11-08 09:06:01 +0000

    Finalize

--- Day3 Treausre Island.py
+++ Day3 Treausre Island.py
@@ -209,11 +209,11 @@ if choice1=='left':
 %#%%%%%#%#%%######%#########%%##%##*%%%*###*###+#%#%%##%%%#%%%##%#%##%%%%%%#%%######%####%%%%%%%%%%
 %%%%%%%####%######***#%###%%%##%##**%###%#%##**#####%%%#%%#%%%#%%%##%####%##%#%%%##%##%####%%%%%%%%
 ''')
-    choice2=input('''You've come to a lake. There is an island in the middle of the lake. Type "Wait" to wait for a boat. Type "Swim" to swim across.''').lower()
+    choice2=input('''You've come to a lake. There is an island in the middle of the lake. Type "Wait" to wait for a boat. Type "Swim" to swim across. ''').lower()
     if choice2=="wait":
         print('''''')
         choice3=input('''You arrive at the island unharmed. There is a house with three doors. One red, one yellow and one blue. Which color 
-do you choose''').lower()
+do you choose. ''').lower()
         if choice3=="red":
             print("It's a room full of fire. Game Over.")
         elif choice3=="yellow":
