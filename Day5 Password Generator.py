1a6ca363cc125a07a7dab57094e0f2bc819f945f
Amit140299 <104049987+Amit140299@users.noreply.github.com>
2023-11-15 15:32:52 +0000

    Day 5 started

--- Day5 Password Generator.py
+++ Day5 Password Generator.py
@@ -1,4 +1,26 @@
-fruits=["Apple","Peach","Pear"]
-for fruit in fruits:
-    print(fruit)
-    print(fruit+"Pie")
+# fruits=["Apple","Peach","Pear"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit+"Pie")
+
+# print(("172 1271 223 121 454").split())
+# studnet_heights=input().split()
+# n=0
+# Total_height=0
+# for height in studnet_heights:
+#     print(height)
+#     Total_height+=int(height)
+#     n+=1
+# Avg_height=Total_height/n
+# print(f"Total Heigth= {Total_height}")
+# print(f"Total Students= {n}")
+# print(f"Avgerage Height= {Avg_height}")
+
+student_scores=input().split()
+index=0
+hightest_score=0
+for score in student_scores:
+    if int(score)>hightest_score:
+        hightest_score=int(score)
+        
+print(hightest_score)
