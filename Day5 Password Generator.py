e3d3993580a733fb7d3868e4f481942fd8c7cb27
Amit140299 <104049987+Amit140299@users.noreply.github.com>
2023-11-15 16:23:22 +0000

    Sign Off

--- Day5 Password Generator.py
+++ Day5 Password Generator.py
@@ -16,11 +16,34 @@
 # print(f"Total Students= {n}")
 # print(f"Avgerage Height= {Avg_height}")
 
-student_scores=input().split()
-index=0
-hightest_score=0
-for score in student_scores:
-    if int(score)>hightest_score:
-        hightest_score=int(score)
-        
-print(hightest_score)
+
+# Maximum from the list and finding index
+# student_scores=input().split()
+# index=0
+# hightest_score=0
+# n=0
+# for score in student_scores:
+#     if int(score)>hightest_score:
+#         hightest_score=int(score)
+#         index=n
+#     n+=1
+# print(hightest_score)
+# print(index)
+
+# for number in range(1,10):
+#     print(number)
+
+# Sum of all he numbers between 1 to 100
+# total=0
+# for number in range(1,101):
+#     total+=number
+# print(total)
+
+# Sum of all the even numbers between user defined numbers
+starting_number=int(input())
+ending_numbers=int(input())
+if starting_number%==0:
+total=0
+for number in range(starting_number,101,2):
+    total+=number
+print(total)
