1526e8976860cf89041d164cc828bb7c13d3144f
Amit140299 <104049987+Amit140299@users.noreply.github.com>
2023-11-19 20:28:03 +0000

    Completed day 5

--- Day5 Password Generator.py
+++ Day5 Password Generator.py
@@ -40,10 +40,72 @@
 # print(total)
 
 # Sum of all the even numbers between user defined numbers
-starting_number=int(input())
-ending_numbers=int(input())
-if starting_number%==0:
-total=0
-for number in range(starting_number,101,2):
-    total+=number
-print(total)
+# total=0
+# starting_number=int(input())
+# if starting_number%2==0:
+#     for number in range(starting_number,101,2):
+#         total+=number
+# print(total)
+
+
+# FizzBuzz Game
+
+# for num in range(1,101):
+#     if num%3==0 and num%5==0:
+#         print("FizzBuzz")
+#     elif num%3==0:
+#         print("Fizz")
+#     elif num%5==0:
+#         print("Buzz")
+#     else:
+#         print(num)
+# Final Project Password Generator
+
+
+# list1=["a",'b','c']
+# list2=['s','g','t',]
+
+# list3=list1+list2
+# print(list3)
+
+import random as rd
+
+letters=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+          'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+          'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+numbers=[ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+symbols=['!','@','#','$','%','^','&','*','(',')','-','_','+']
+
+print("Welcome to the PyPassword Generator!")
+Pass_level=input("What level of password do you want to generate? Type 'Easy or 'Hard' \n").lower()
+
+nr_letters=int(input("How many letters would you like to have in your password?\n"))
+nr_numbers=int(input("How many numbers would you like to have in your password?\n"))
+nr_symbols=int(input("How many symbols would you like to have in your password?\n"))
+
+password=""
+
+if Pass_level=='easy':
+    for i in range(0,nr_letters):
+        password+=rd.choice(letters)
+
+    for i in range(0,nr_symbols):
+        password+=rd.choice(symbols)
+
+    for i in range(0,nr_numbers):
+        password+=rd.choice(numbers)
+
+    print(password)
+
+elif Pass_level=="hard":
+    total_length=nr_letters+nr_numbers+nr_symbols
+    combined_list=letters+symbols+numbers
+
+    for i in range(0,total_length):
+        password+=rd.choice(combined_list)
+
+    print(password)
+else:
+    print("Choose a valid option.")
