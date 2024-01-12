75053bf259ae9ed546e6bc09d6c1a10ef19cc034
Amit140299 <104049987+Amit140299@users.noreply.github.com>
2024-01-12 19:12:07 +0000

    Day 7 completed half

--- Day7 Hangman Game.py
+++ Day7 Hangman Game.py
@@ -1,4 +1,24 @@
+from operator import pos
 import random as rd
-random_number=rd.randint(0,2)
-work_list=['ardvark','baboon','camel']
-print(work_list[random_number])
+
+from sympy import false, true
+word_list=['ardvark','baboon','camel']
+chosen_word=rd.choice(word_list)
+
+Display=[]
+for letter in range(len(chosen_word)):
+    Display+="_"
+print(Display)
+
+End_of_game=False
+while not End_of_game:
+    guess=input("Guess a letter:").lower()
+    for position in range(len(chosen_word)):
+        if guess==chosen_word[position]:
+            Display[position]=guess
+    print(Display)
+
+    if "_" in Display:
+        End_of_game=False
+    else:
+        print("You Win!")
