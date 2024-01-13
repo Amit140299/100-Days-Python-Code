b2f7d67c30bd69c6f10519adba6eccb71687f6c6
Amit140299 <104049987+Amit140299@users.noreply.github.com>
2024-01-13 10:25:52 +0000

    Cokmpleted project 7

--- Day7 Hangman Game.py
+++ Day7 Hangman Game.py
@@ -1,63 +1,10 @@
 import random as rd
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+from Day7_Hangman_words import word_list
 
-word_list=['ardvark','baboon','camel']
+from Day7_Hangman_art import stages,logo
+
+print(logo)
 chosen_word=rd.choice(word_list)
 
 Display=[]
@@ -65,17 +12,20 @@ Lives=6
 
 for letter in range(len(chosen_word)):
     Display+="_"
-print(Display)
+print(f"{' '.join(Display)}")
 
 End_of_game=False
 while not End_of_game and Lives!=0:
     guess=input("Guess a letter:").lower()
+    if guess in Display:
+        print(f"You have already guessed {guess}")
     for position in range(len(chosen_word)):
         if guess==chosen_word[position]:
             Display[position]=guess
     if guess not in chosen_word:
+        print(f"You have guessed this {guess} letter that is not in the word. You lose a life.")
         Lives-=1
-    print(Display)
+    print(f"{' '.join(Display)}")
     print(stages[Lives])
     
     if "_" in Display:
