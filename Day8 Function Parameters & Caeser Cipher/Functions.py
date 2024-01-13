b0828242f69393b596a9fade1b67f63587480128
Amit140299 <104049987+Amit140299@users.noreply.github.com>
2024-01-13 10:45:34 +0000

    satrted day 8

---
+++ Day8 Function Parameters & Caeser Cipher/Functions.py
@@ -0,0 +1,15 @@
+# def greet():
+#     print("Hello")
+#     print("How are you doing?")
+#     print("Isn't the weather nice today?")
+
+# def greet_with_name(name):
+#     print("Hello")
+#     print(f"How are you doing {name}?")
+#     print(f"Isn't the weather nice today {name}?")
+
+def greet_with(name,location):
+    print(f"Hello {name}.")
+    print(f"How is it like in {location}")
+
+greet_with("Amit","Mumbai")
