5b818ade727327814232d0602911d8998355bb76
Amit140299 <104049987+Amit140299@users.noreply.github.com>
2024-01-22 18:36:06 +0000

    day 9 alomost completed

--- Day 9 Python Dictionaries and Nesting & Silent Auction /Nesting List and a dictionary.py
+++ Day 9 Python Dictionaries and Nesting & Silent Auction /Nesting List and a dictionary.py
@@ -1,4 +1,16 @@
 # Nesting
-capitals={"France":"Paris","Germany":"Berlin"}
+capitals={"France":"Paris",
+          "Germany":"Berlin"}
 
-print{capitals} 
+print(capitals)
+
+# Nesting a list in a dictionary
+
+travel_log={"France":["Paris","Lille","Dijon"],
+            "Germany":["Berlin","Hamburg","Stuttgart"]}
+print(travel_log)
+
+# Nesting a Dictionary in a dictionary
+travel_log={"France":{"cities_visited":{"Paris":14,"Lille":3,"Dijon":2}},
+            "Germany":{"cities_visitied":{"Berlin","Hamburg","Stuttgart"}}}
+print(travel_log["France"]["cities_visited"]["Paris"])
---
+++ Day 9 Python Dictionaries and Nesting & Silent Auction /Secrete Auction.py
@@ -0,0 +1,31 @@
+from replit import clear
+from art import Auction_logo
+print(Auction_logo)
+
+bids={}
+bidding_finnished=False
+
+def find_highest_bidder(bidding_record):
+    # bidding_record={"Amit":123,"Bobby":321}
+    highest_bid=0
+    winner=""
+    for bidder in bidding_record:
+        bid_amount=bidding_record[bidder]
+        if bid_amount>highest_bid:
+            highest_bid=bid_amount
+            winner=bidder
+    print(f"The winner is {winner} with a bid of ₹{highest_bid}.")
+
+while not bidding_finnished:
+    name=input("What's your name?: ")
+    price=int(input("What is your bid? ₹"))
+    bids[name]=price
+    should_continue=input("Are there any other bidders? Type 'yes' or 'no'. \n").lower()
+    if should_continue=="no":
+        bidding_finnished=True
+        clear()
+        find_highest_bidder(bids)
+    elif should_continue=="yes":
+        clear()
+    else:
+        print("Please provide a proper input.")
